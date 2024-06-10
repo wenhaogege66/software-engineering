823bfe88d423a2a896b4e2bc3681a89225d6f94a
2211571350 <161669907+2211571350@users.noreply.github.com>
2024-06-10 08:03:33 +0800

    Add files via upload

---
+++ frontend/src/online_user/online_user.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="main">
+      <el-container>
+        <el-aside class="aside">
+          <el-menu active-text-color="#ffd04b" background-color="#444444" default-active="1" text-color="#fff"
+                   style="height:100%; width: 100%; overflow: hidden" :router="true">
+            <div style="color: white; background-color: #181818;
+            width: 100%; height: 10vh; display: flex; align-items: center; justify-content: center;">
+              互联网个人银行
+            </div>
+
+            <el-menu-item index="/online_user/home">
+              <el-icon>
+                <Avatar />
+              </el-icon>
+              <span>首页</span>
+            </el-menu-item>
+            <el-menu-item index="/online_user/personal">
+              <el-icon>
+                <User />
+              </el-icon>
+              <span>密码修改</span>
+            </el-menu-item>
+            <el-menu-item index="/online_user/account">
+              <el-icon>
+                <User />
+              </el-icon>
+              <span>账户信息</span>
+            </el-menu-item>
+            <el-menu-item index="/online_user/record">
+              <el-icon>
+                <User />
+              </el-icon>
+              <span>交易记录</span>
+            </el-menu-item>
+            <div style="height: 30px"></div>
+  <!--          <a href="/" style="margin-left: 40px;">-->
+  <!--            <el-button type="danger">-->
+  <!--              退出-->
+  <!--            </el-button>-->
+  <!--          </a>-->
+          </el-menu>
+  
+        </el-aside>
+        <el-container>
+          <el-header style="height: 0vh;"></el-header>
+          <el-main style="height: 100%; width: 100%; ">
+            <el-scrollbar height="100%">
+              <RouterView class="content" style="height: 100vh; max-height: 100%; background-color: white; color: black;" />
+            </el-scrollbar>
+          </el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+    }
+  }
+  </script>
+  
+  <style scoped>
+  #app {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #dcdcdc;
+    width: 100vw;
+    height: 100vh;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .main {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    min-height: 100%;
+    height: auto;
+    background-color: #dcdcdc;
+  
+  }
+  
+  .title {
+    background-color: #ffffff;
+    height: 60px;
+  }
+  
+  .aside {
+    min-height: 100vh;
+    width: 200px;
+    background-color: black;
+  }
+  </style>
+  
---
+++ frontend/src/online_user_administrator/components/black.vue
@@ -0,0 +1,51 @@
+<template>
+    <el-scrollbar height="100%" style="width: 100%; height: 100%">
+      <div style="margin-top: 20px; margin-left:20px; font-size: 2em; font-weight: bold;">
+        黑名单管理
+      </div>
+      <div style="width:45%;margin:0 auto; padding-top:5vh;">
+            <el-button type="primary" @click="DepositVisible = true">添加黑名单</el-button>
+      </div>
+      <el-table :data="tableData" style="width: 70% ;margin-left:15%">
+        <el-table-column fixed prop="date" label="用户名" width="300" />
+        <el-table-column prop="name" label="手机号" width="300" />
+        <el-table-column prop="state" label="身份证号" width="300" />
+        <el-table-column fixed="right" label="Operations" width="150">
+      <template #default>
+        <el-button link type="primary" size="small" @click="handleClick">
+          移除
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+
+
+      <el-dialog v-model="DepositVisible" title="添加黑名单" width="30%" align-center :before-close="handleClose">
+          <div style="margin-left: 2vw; font-weight: bold; font-size: 1rem; margin-top: 20px; ">
+              用户名：
+              <!-- <el-input v-model="newDepositInfo.account_id" style="width: 12.5vw;" clearable /> -->
+          </div>
+
+          <template #footer>
+              <span>
+                  <el-button @click="DepositVisible = false">确认</el-button>
+              </span>
+          </template>
+      </el-dialog>
+  
+    </el-scrollbar>
+  </template>
+  <script lang="ts" setup>
+import { ref } from 'vue'
+
+const DepositVisible = ref(false)
+
+const handleClick = () => {
+  console.log('click')
+}
+
+  </script>
+  
+  <style scoped>
+
+  </style>
